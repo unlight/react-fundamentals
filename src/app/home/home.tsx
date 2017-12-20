ecbad707e32864a10ec7fd8451748899a67c78be
roman <s91630277@gmail.com>
2017-12-21 00:44:58 +0400

    Fragment and some examples

--- src/app/home/home.tsx
+++ src/app/home/home.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
+import { Fragment } from 'react';
 
 export class Home extends React.Component<any, any> {
 
     render() {
-        return (
+        return <Fragment>
             <div className="pt-running-text">
                 <h2>Home page</h2>
             </div>
-        );
+            <p>This is home page</p>
+        </Fragment>;
     }
 }
